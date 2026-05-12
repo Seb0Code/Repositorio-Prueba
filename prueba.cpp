614c1810add29207895efa3d702008b505f8f88d
Seb0Code <sebastiangutierrezc7@gmail.com>
2026-05-12 19:21:53 -0400

    feat(prueba.cpp): Agregar funcion -Agregar una funcion para que el usuario ingrese un numero

--- prueba.cpp
+++ prueba.cpp
@@ -3,4 +3,14 @@
 using std::cin;
 using std::cout;
 
-int main() { cout << "Este es un archivo de prueba para git\n"; }
+int main() {
+    cout << "Este es un archivo de prueba para git\n";
+    cout << "Estoy agregando mas lineas y cout jajaja\n";
+    int num;
+    cout << "Ingrese un numero: ";
+    cin >> num;
+    if (cin.fail()) {
+        cin.clear();
+        cin.ignore(10000, '\n');
+    }
+}
